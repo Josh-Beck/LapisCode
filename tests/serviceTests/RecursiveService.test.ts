0deae0f3b01fe526e10dffd30fd957e7cd7af9ed
Josh-Beck <JoshuaThomasBeck@gmail.com>
2023-03-23 15:47:35 -0400

    Additional test case

--- tests/serviceTests/RecursiveService.test.ts
+++ tests/serviceTests/RecursiveService.test.ts
@@ -1,16 +1,38 @@
 import {describe, expect, test} from '@jest/globals';
 import { resourceLimits } from 'worker_threads';
+import * as RecursiveService from "../../src/services/RecursiveService";
 
-describe('GitHub Service Tests', () => {
+describe('Recursion Service Tests', () => {
     
-    test('GitH URL Builder', () => {
-
-        let result:GithubNode = await GithubService.getGithubFileFromWebsiteURL("https://github.com/Josh-Beck/LapisCode-Test-Code/blob/main/unitTests/GithubService/GithubURLTest.md");
-
-        expect(result.name).toBe("GithubURLTest.md");
-        
-
+    test('Recursion Max check (5)', () => {
+        expect(RecursiveService.recursionMaximumCheck(5)).toBe(false);
     });
 
+    test('Recursion Max check (1000)', () => {
+        expect(RecursiveService.recursionMaximumCheck(1000)).toBe(true);
+    });
 
+    test('Generic Recursion Function', () => {
+        let testResults:string[] = [];
+        RecursiveService.recurseObject(
+            JSON.parse(`{
+                "x":"y",
+                "z":["a"]
+            }`),
+            function(node:Object):boolean {
+                return typeof node === "string" && node === "y";
+            },
+            function(node:string):void {
+                testResults.push(node);
+            },
+            0
+        )
+        
+        expect(testResults[0]).toBe("y")
+        expect(testResults.length).toBe(1)
+    });
+
+    // test('FileNode Recursion Function', () => {
+    //     //TODO
+    // });
 }); 
